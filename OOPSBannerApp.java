305620487a64d5a55d25d731089a33bb107191ae
fv001 <fv8956@srmist.edu.in>
2026-02-23 21:08:02 +0530

    UC6: Refactor banner logic into static helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,36 +1,51 @@
-/**
- * UC3
- *
- * UC3: Render OOPS as a Banner using String.join().
- * This program refactors UC2 by replacing string concatenation
- * with the String.join() method for better memory efficiency
- * and readability.
- *
- * @author Fadil
- * @version 1.0
- */
 public class OOPSBannerApp {
 
-    /**
-     * Main method – entry point of the Java application
-     *
-     * @param args command-line arguments
-     */
+    public static String[] getOPattern() {
+        return new String[] {
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[] {
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[] {
+            " *** ",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            " *** "
+        };
+    }
+
     public static void main(String[] args) {
 
 
-        String[] bannerLines = {
-            String.join("  ", " *** ", " *** ", "**** ", " *** "),
-            String.join("  ", "*   *", "*   *", "*   *", "*    "),
-            String.join("  ", "*   *", "*   *", "*   *", "*    "),
-            String.join("  ", "*   *", "*   *", "**** ", " *** "),
-            String.join("  ", "*   *", "*   *", "*    ", "    *"),
-            String.join("  ", "*   *", "*   *", "*    ", "    *"),
-            String.join("  ", " *** ", " *** ", "*    ", " *** ")
-        };
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        for (String line : bannerLines) {
-            System.out.println(line);
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + "  " + oPattern[i] + "  " + pPattern[i] + "  " + sPattern[i]);
         }
     }
 }
