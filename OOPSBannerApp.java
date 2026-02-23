b4fb23cf4780a09ce2e63548725f3affe90194fa
fv001 <fv8956@srmist.edu.in>
2026-02-23 20:31:45 +0530

    feature(UC1): print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,26 @@
+/**
+ * OOPS Banner App
+ *
+ * UC1: Print OOPS to Console
+ *
+ * This application prints the literal text "OOPS"
+ * to the console as part of the first use case.
+ *
+ * @author developer
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Entry point of the application
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        printOOPS();
+    }
+
+    static void printOOPS() {
+        System.out.println("OOPS");
+    }
+}
