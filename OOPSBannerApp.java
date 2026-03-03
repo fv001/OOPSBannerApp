6e2ad60bde44521d42fcede24158dcd80e55d54a
fv001 <fv8956@srmist.edu.in>
2026-03-03 12:10:07 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,5 @@
-/**
- * OOPS Banner App
- *
- * UC1: Print OOPS to Console
- *
- * This application prints the literal text "OOPS"
- * to the console as part of the first use case.
- *
- * @author developer
- * @version 1.0
- */
 public class OOPSBannerApp {
 
-    /**
-     * Entry point of the application
-     *
-     * @param args command-line arguments
-     */
     public static void main(String[] args) {
         printOOPS();
     }
