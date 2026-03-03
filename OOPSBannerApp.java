eea16381341e737fe3b98f6691660306aec29ec8
fv001 <fv8956@srmist.edu.in>
2026-03-03 12:11:07 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,6 @@
-/**
- * UC3
- *
- * UC3: Render OOPS as a Banner using String.join().
- * This program refactors UC2 by replacing string concatenation
- * with the String.join() method for better memory efficiency
- * and readability.
- *
- * @author Fadil
- * @version 1.0
- */
+
 public class OOPSBannerApp {
 
-    /**
-     * Main method – entry point of the Java application
-     *
-     * @param args command-line arguments
-     */
     public static void main(String[] args) {
 
 
