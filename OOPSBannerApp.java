5dd88fc110b65cbf1a17cf90d2329c1326d59a57
fv001 <fv8956@srmist.edu.in>
2026-03-03 12:10:27 +0530

    Remove comments from OOPSBannerApp.java
    Removed header comments from OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,4 @@
-/**
- * OOPS Banner App
- *
- * UC2: Render OOPS as Banner using Print Statements
- *
- * @author YourName
- * @version 2.0
- */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
